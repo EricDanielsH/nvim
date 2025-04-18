3387278512fc6f5c4786c40986bf11230068e05e
Eric Daniels <e.danielshuaman@lancaster.ac.uk>
2025-04-19 00:14:12 +0100

    feat: move explorer to right side

---
+++ lua/plugins/explorer-right.lua
@@ -0,0 +1,15 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    picker = {
+      sources = {
+        explorer = {
+          layout = { layout = { position = "right" } },
+        },
+      },
+    },
+  },
+}
