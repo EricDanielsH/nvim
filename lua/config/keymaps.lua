f8d9f9bffd751753dd3de5d61f9259d20726aa07
Eric Daniels <e.danielshuaman@lancaster.ac.uk>
2025-05-26 19:33:15 +0100

    feat: compile and run c++ with F5

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -42,3 +42,55 @@ end, { desc = "Terminal (Root Dir)" })
 
 -- Terminal Mappings
 map("t", "<C-\\>", "<cmd>close<cr>", { desc = "Hide Terminal" })
+
+-- Compile and run current C++ file on macOS using iTerm2
+local function CompileAndRun()
+  local file = vim.fn.expand("%:p") -- full path to current file
+  if file:match("%.cpp$") then
+    local exe = vim.fn.fnamemodify(file, ":r") -- remove .cpp to get full exe path
+    vim.cmd("write")
+
+    local compile_cmd = string.format("g++ -std=c++11 -Wall -Wextra -Wpedantic -O2 -o %q %q", exe, file)
+    vim.fn.system(compile_cmd)
+
+    if vim.v.shell_error == 0 then
+      local dir = vim.fn.fnamemodify(exe, ":h") -- get directory of the binary
+      local bin = vim.fn.fnamemodify(exe, ":t") -- get filename only
+
+      local iterm_script = string.format(
+        [[
+osascript <<EOF
+tell application "iTerm"
+  activate
+  try
+    set newWindow to (create window with default profile)
+  on error
+    set newWindow to (create tab with default profile)
+  end try
+  tell current session of newWindow
+    write text "cd %s && ./%s; echo \\"\nPress enter to exit...\\"; read"
+  end tell
+end tell
+EOF
+]],
+        dir,
+        bin
+      )
+
+      vim.fn.system(iterm_script)
+      vim.cmd("redraw!")
+    else
+      vim.notify("Compilation failed", vim.log.levels.ERROR)
+    end
+  else
+    vim.notify("Not a C++ file", vim.log.levels.WARN)
+  end
+end
+
+-- Keymaps
+vim.keymap.set("n", "<F5>", CompileAndRun, { desc = "Compile and Run C++ (iTerm)" })
+vim.keymap.set("n", "<F9>", function()
+  vim.cmd("write")
+  vim.cmd("!clear")
+  CompileAndRun()
+end, { desc = "Clear, Compile, and Run C++ (iTerm)" })
